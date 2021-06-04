f16758b534e55ef2cda615d7762ae609c272601d
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-06-04 13:37:29 +0600

    debugging

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -140,7 +140,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 
 
